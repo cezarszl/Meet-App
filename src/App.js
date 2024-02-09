04688c5c792ebbcd8d818a8d62e6e42dd88be160
cezarszl <cezarszlfedora@gmail.com>
2024-02-09 14:44:49 +0100

    update

--- src/App.js
+++ src/App.js
@@ -1,39 +1,44 @@
-import './App.css';
-import { useEffect, useState } from 'react';
-import CitySearch from './components/CitySearch';
-import EventList from './components/EventList';
-import NumberOfEvents from './components/NumberOfEvents';
-import { getEvents, extractLocations } from './api';
-import { InfoAlert, ErrorAlert, WarningAlert } from './components/Alert';
+import "./App.css";
+import CitySearch from "./components/CitySearch";
+import EventList from "./components/EventList";
+import NumberOfEvents from "./components/NumberOfEvents";
+import CityEventsChart from "./components/CityEventsCharts"
+import EventGenresChart from "./components/EventGenresChart";
+import { useEffect, useState } from "react";
 
+import { extractLocations, getEvents } from "./api";
+
+import { ErrorAlert, InfoAlert, WarningAlert } from "./components/Alert";
 
 const App = () => {
-
-  const [events, setEvents] = useState([]);
-  const [currentNOE, setCurrentNOE] = useState(32);
   const [allLocations, setAllLocations] = useState([]);
+  const [currentNOE, setCurrentNOE] = useState(32);
+  const [events, setEvents] = useState([]);
   const [currentCity, setCurrentCity] = useState("See all cities");
   const [infoAlert, setInfoAlert] = useState("");
   const [errorAlert, setErrorAlert] = useState("");
   const [warningAlert, setWarningAlert] = useState("");
 
   useEffect(() => {
+    //check to render warning alert
+
     if (navigator.onLine) {
       setWarningAlert("");
     } else {
-      setWarningAlert("You are offline. The displayed list may not be up to date.");
+      setWarningAlert("you are currently in offline mode");
     }
     fetchData();
   }, [currentCity, currentNOE]);
 
   const fetchData = async () => {
     const allEvents = await getEvents();
-    const filteredEvents = currentCity === "See all cities" ?
-      allEvents :
-      allEvents.filter(event => event.location === currentCity)
+    const filteredEvents =
+      currentCity === "See all cities"
+        ? allEvents
+        : allEvents.filter((event) => event.location === currentCity);
     setEvents(filteredEvents.slice(0, currentNOE));
     setAllLocations(extractLocations(allEvents));
-  }
+  };
 
   return (
     <div className="App">
@@ -42,11 +47,29 @@ const App = () => {
         {errorAlert.length ? <ErrorAlert text={errorAlert} /> : null}
         {warningAlert.length ? <WarningAlert text={warningAlert} /> : null}
       </div>
-      <CitySearch allLocations={allLocations} setCurrentCity={setCurrentCity} setInfoAlert={setInfoAlert} />
-      <NumberOfEvents setCurrentNOE={setCurrentNOE} setErrorAlert={setErrorAlert} />
+      <div className="title-box">
+        <p className="title">Meet App 📍</p>
+        <p className="subtitle">
+          Search tech events near you using Careerfoundrys calendar!
+        </p>
+      </div>
+      <CitySearch
+        allLocations={allLocations}
+        setCurrentCity={setCurrentCity}
+        setInfoAlert={setInfoAlert}
+      />
+      <NumberOfEvents
+        setCurrentNOE={setCurrentNOE}
+        setErrorAlert={setErrorAlert}
+      />
+
+      <div className="charts-container">
+        <EventGenresChart events={events} />
+        <CityEventsChart allLocations={allLocations} events={events} />
+      </div>
       <EventList events={events} />
     </div>
   );
-}
+};
 
 export default App;
