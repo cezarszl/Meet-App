73813a4341ee3fbd95d3d63b4246493247d98eab
cezarszl <cezarszlfedora@gmail.com>
2024-02-11 13:23:34 +0100

    update

--- src/api.js
+++ src/api.js
@@ -1,5 +1,5 @@
 import mockData from './mock-data';
-import { NProgress } from 'nprogress';
+import NProgress from 'nprogress';
 
 export const extractLocations = (events) => {
     const extractedLocations = events.map((event) => event.location);
